9ce367b3ccc19a63b27b7098fe115d6502442a27
Sharans23 <sharan.shetty2309@gmail.com>
2025-03-24 12:53:55 +0530

    deployment changes

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -8,7 +8,7 @@ export default function Layout() {
   const [sidebarOpen, setSidebarOpen] = useState(true);
 
   return (
-    <div className="flex h-screen overflow-y-hidden" style={{height: '380vh'}}>
+    <div className="flex h-screen overflow-y-hidden" style={{height: '375vh'}}>
       <Sidebar open={sidebarOpen} setOpen={setSidebarOpen} />
 
       <div className="flex flex-1 flex-col overflow-y-hidden">
